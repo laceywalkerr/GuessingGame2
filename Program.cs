d4bcd1a3adc1e6edc17a5ac7bef6fa238f06412c
Lacey Walker <bluelarksong@gmail.com>
2020-11-16 09:35:50 -0600

    first commit, phase one completed

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GuessingGame2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Can you guess the number I'm thinking of?");
+            string number = Console.ReadLine();
+            Console.WriteLine($"You guessed {number}? Oh, Okay.");
+        }
+    }
+}
