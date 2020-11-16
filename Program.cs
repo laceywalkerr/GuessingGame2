45cb7e397399237c1d60be4d7c52a638d3ef69c6
Lacey Walker <bluelarksong@gmail.com>
2020-11-16 10:48:42 -0600

    converting readline string to int, now have to count down attempts properly.

--- Program.cs
+++ Program.cs
@@ -6,18 +6,20 @@ namespace GuessingGame2
     {
         static void Main(string[] args)
         {
-            string SecretNumber = "42";
-            Console.WriteLine(SecretNumber);
+            int secretNumber = 42;
+            Console.WriteLine(secretNumber);
 
-            Console.Write("Can you guess the number I'm thinking of? ");
-            string UserNumber = Console.ReadLine();
-            // Console.WriteLine($"You guessed {UserNumber}? Oh, Okay.");
+            int attempts = 4;
 
-            if (SecretNumber == UserNumber)
+            Console.Write($"Can you guess the number I'm thinking of? You have { attempts - 1} guesses left!");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine($"You guessed {userNumber}? Oh, Okay.");
+
+            if (secretNumber == userNumber)
             {
                 Console.WriteLine("SUCCESS! You got it, dude!");
             }
-            else if (SecretNumber != UserNumber)
+            else if (secretNumber != userNumber)
             {
                 Console.WriteLine("Uhmm.. nah man. Try again.");
             }
