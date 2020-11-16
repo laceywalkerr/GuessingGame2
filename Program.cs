283adeedb3f083b3a373f8cf89a3a884c9339047
Lacey Walker <bluelarksong@gmail.com>
2020-11-16 11:19:34 -0600

    count down now works properly

--- Program.cs
+++ Program.cs
@@ -11,17 +11,20 @@ namespace GuessingGame2
 
             int attempts = 4;
 
-            Console.Write($"Can you guess the number I'm thinking of? You have { attempts - 1} guesses left!");
-            int userNumber = Convert.ToInt32(Console.ReadLine());
-            // Console.WriteLine($"You guessed {userNumber}? Oh, Okay.");
+            for (int i = 0; i < attempts; i++)
+            {
 
-            if (secretNumber == userNumber)
-            {
-                Console.WriteLine("SUCCESS! You got it, dude!");
-            }
-            else if (secretNumber != userNumber)
-            {
-                Console.WriteLine("Uhmm.. nah man. Try again.");
+                Console.Write($"Can you guess the number I'm thinking of? You have { attempts - i - 1} guesses to get it right! ");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                // Console.WriteLine($"You guessed {userNumber}? Oh, Okay.");
+                if (secretNumber == userNumber)
+                {
+                    Console.WriteLine("SUCCESS! You got it, dude!");
+                }
+                else if (secretNumber != userNumber)
+                {
+                    Console.WriteLine("Uhmm.. nah man. Try again.");
+                }
             }
 
         }
