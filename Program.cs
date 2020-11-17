209a8ef4e11d4e679ff19f13773f36e4da32f9c0
Lacey Walker <bluelarksong@gmail.com>
2020-11-16 18:42:12 -0600

    cheaters never win, except today. phase8 complete.

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace GuessingGame2
             while (attempts < attemptsAllowed || attemptsAllowed == -1)
             {
                 // the $ in front of the string to summon the info is a "string interpolation"
-                Console.Write($"Can you guess the number I'm thinking of? You have ({ attemptsAllowed - 1}) guesses to get it right!  ");
+                Console.Write($"Can you guess the number I'm thinking of? You have ({ attemptsAllowed - attempts }) guesses to get it right!  ");
                 string userResponse = Console.ReadLine();
                 int userNumber = int.Parse(userResponse);
                 Console.WriteLine($"Number guessed: {userNumber}");
@@ -37,6 +37,15 @@ namespace GuessingGame2
                         Console.WriteLine("Try guessing a little lower!");
                     }
                     attempts++;
+
+                    if (attemptsAllowed == -1)
+                    {
+                        Console.WriteLine("Take your time :] ");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You have {attemptsAllowed - attempts} guesses left");
+                    }
                 }
             }
         }
@@ -46,7 +55,7 @@ namespace GuessingGame2
             int attempts = 0;
             while (attempts == 0)
             {
-                Console.Write("What difficulty would you like to select? (easy, medium, hard)");
+                Console.Write("What difficulty would you like to select? (easy, medium, hard): ");
                 string userResponse = Console.ReadLine().ToLower();
 
                 switch (userResponse)
@@ -63,9 +72,9 @@ namespace GuessingGame2
                         attempts = 4;
                         break;
 
-                        // case ("cheater"):
-                        //     attempts = -1;
-                        //     break;
+                    case ("cheater"):
+                        attempts = -1;
+                        break;
 
                     default:
                         break;
